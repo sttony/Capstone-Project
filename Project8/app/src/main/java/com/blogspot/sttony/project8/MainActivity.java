c1b32441db4e896397fe23efd0cf9a0bf08c0af3
sttony <tong.tongli@gmail.com>
2015-12-16 22:20:33 -0800

    Button -> add add viewpager

--- Project8/app/src/main/java/com/blogspot/sttony/project8/MainActivity.java
+++ Project8/app/src/main/java/com/blogspot/sttony/project8/MainActivity.java
@@ -3,6 +3,7 @@ package com.blogspot.sttony.project8;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
