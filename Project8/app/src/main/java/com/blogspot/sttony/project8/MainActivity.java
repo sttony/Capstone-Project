dea4ba089d13ac4a9ec556bfc618af0ad541fb58
sttony <tong.tongli@gmail.com>
2015-12-16 23:09:53 -0800

    Try to add ViewPager

--- Project8/app/src/main/java/com/blogspot/sttony/project8/MainActivity.java
+++ Project8/app/src/main/java/com/blogspot/sttony/project8/MainActivity.java
@@ -1,8 +1,13 @@
 package com.blogspot.sttony.project8;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,12 +18,36 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     ViewPager mViewPager;
+    MainActivitySectionsPagerAdapter mMAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // View pager
+        mMAdapter = new MainActivitySectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.mainpager);
+        mViewPager.setAdapter(mMAdapter);
+
+//        final ActionBar actionBar = getActionBar();
+//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+//        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+//            @Override
+//            public void onPageSelected(int position) {
+//                actionBar.setSelectedNavigationItem(position);
+//            }
+//        });
+//        for (int i = 0; i < mMAdapter.getCount(); i++) {
+//            // Create a tab with text corresponding to the page title defined by the adapter.
+//            // Also specify this Activity object, which implements the TabListener interface, as the
+//            // listener for when this tab is selected.
+//            actionBar.addTab(
+//                    actionBar.newTab()
+//                            .setText(mMAdapter.getPageTitle(i))
+//                            .setTabListener(this));
+//        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,5 +79,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class MainActivitySectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public MainActivitySectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                case 0:
+                    // The first section of the app is the most interesting -- it offers
+                    // a launchpad into the other demonstrations in this example application.
+                    return new TasksFragment();
+
+                default:
+                    // The other sections of the app are dummy placeholders.
+                    Fragment fragment = new GoalsFragment();
+//                    Bundle args = new Bundle();
+//                    args.putInt(GoalsFragment.ARG_SECTION_NUMBER, i + 1);
+//                    fragment.setArguments(args);
+                    return fragment;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Section " + (position + 1);
+        }
     }
 }
