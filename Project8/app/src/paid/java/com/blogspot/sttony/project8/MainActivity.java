9f690b44ff8aad31ef98f2856f6855198725640b
sttony <tong.tongli@gmail.com>
2016-01-21 16:14:18 -0800

    Add setting activity

--- Project8/app/src/paid/java/com/blogspot/sttony/project8/MainActivity.java
+++ Project8/app/src/paid/java/com/blogspot/sttony/project8/MainActivity.java
@@ -95,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
